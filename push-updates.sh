bdcf2366500d66bccd4d03cad3de5c6d67b3f147
root <root@homeassistant.local.hass.io>
2022-11-13 21:19:18 +0100

    Uploaded by push-updates on 13-11-2022 21:19:18

--- push-updates.sh
+++ push-updates.sh
@@ -1,4 +1,5 @@
 cd /config
+git config core.sshCommand "ssh -i /root/config/.ssh/id_rsa -o UserKnownHostsFile=/root/config/.ssh/known_hosts -F /dev/null"
 git add .
 git commit -m "Uploaded by push-updates on `date +'%d-%m-%Y %H:%M:%S'`"
 git push -u origin master
